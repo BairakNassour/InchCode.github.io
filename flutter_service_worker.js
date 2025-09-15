99545fd01cbf76ea553fd3e231af3fb25c1965f6
BairakNassour <103553663+BairakNassour@users.noreply.github.com>
2025-09-15 12:39:54 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -148,7 +148,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9ad16e1198d994ca2e30f41f3b7d9541
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4c92f2b8ea16f9674837159b4dc38ba5",
+"flutter_bootstrap.js": "8b2936bcbc6999511c9dab8813708a65",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
